ea6acc3a8fea1f350f59d22f7c8c25107d673599
dadonc <3952821+dadonc@users.noreply.github.com>
2023-09-15 21:33:29 +0200

    tagtree chevron

--- svelte/components/left/LeftTagTree.svelte
+++ svelte/components/left/LeftTagTree.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
   import LeftSingleTag from "./LeftSingleTag.svelte";
+  import ChevronLeft from "../../assets/feather/ChevronLeft.svelte";
 
   export let tagTree: any;
   export let indent: number;
+
+  const rotateChevron = (e: Event) => {
+    const target = e.target as HTMLElement;
+    const svg = target.children[0];
+    if (svg.nodeName !== "svg") return;
+    if (svg.classList.contains("-rotate-90")) {
+      svg.classList.remove("-rotate-90");
+      svg.classList.add("rotate-180");
+    } else {
+      svg.classList.add("-rotate-90");
+      svg.classList.remove("rotate-180");
+    }
+  };
 </script>
 
 {#each Object.keys(tagTree) as key}
@@ -11,13 +25,14 @@
     style={key !== "_tags" ? `margin-left: ${indent * 1.5}rem` : ""}
   >
     <div
-      class="pl-1 no-select"
+      class="pl-1 select-none"
       on:keydown={(e) => {
         if (e.target) {
           e.stopPropagation();
           if (e.key === "Enter") {
             // @ts-ignore
             e.target.classList.toggle("classToKeep");
+            rotateChevron(e);
           }
         }
       }}
@@ -26,11 +41,13 @@
           e.stopPropagation();
           // @ts-ignore
           e.target.classList.toggle("classToKeep");
+          rotateChevron(e);
         }
       }}
     >
       {#if key !== "_tags"}
-        v {key}
+        <ChevronLeft className="h-2 w-2 inline-block -rotate-90" />
+        {key}
       {/if}
       {#if Array.isArray(tagTree[key]) && key === "_tags"}
         {#each tagTree[key] as tag}
