a35da39d19e3f89f4405bd37c20f5796f2b6204a
dadonc <3952821+dadonc@users.noreply.github.com>
2024-03-14 08:38:32 +0100

    close button for preview modal

--- svelte/assets/feather/Plus.svelte
+++ svelte/assets/feather/Plus.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  export let className = "";
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
   fill="none"
@@ -5,7 +9,7 @@
   stroke-linecap="round"
   stroke-linejoin="round"
   stroke-width="2"
-  class="feather feather-plus"
+  class={"feather feather-plus " + className}
   viewBox="4 4 16 16"
 >
   <path d="M12 5v14M5 12h14" />
--- svelte/components/main/PreviewModal.svelte
+++ svelte/components/main/PreviewModal.svelte
@@ -2,6 +2,7 @@
   import Modal from "../Modal.svelte";
   import { type SingleItem } from "../../stores/items";
   import DetailView from "./DetailView.svelte";
+  import Plus from "../../assets/feather/Plus.svelte";
   export let isOpen = false;
   export let item: SingleItem;
 </script>
@@ -13,7 +14,22 @@
       isOpen = false;
     }}
   >
-    <div slot="body" class="bg-base-100" style="height: calc(100vh - 4rem)">
+    <div
+      slot="body"
+      class="relative bg-base-100"
+      style="height: calc(100vh - 4rem)"
+    >
+      <button
+        title="Close preview modal"
+        on:click={() => {
+          console.log("clicked");
+          isOpen = false;
+        }}
+        class="absolute text-base-content right-4 top-4"
+        style="-webkit-app-region: no-drag;"
+      >
+        <Plus className="w-6 h-6 rotate-45" />
+      </button>
       <DetailView {item} isSpacePreview={true} />
     </div>
   </Modal>
